2af7d7e69dbbf51af3ced12da82829bdb4534469
Jessica Song <jessicasong1998@gmail.com>
2019-04-18 09:53:09 -0400

    in class assignment

---
+++ inclassassignment/script.js
@@ -0,0 +1,17 @@
+function countS(){
+  var stuff  = document.getElementsByTagName('p')
+  var num = stuff.length
+  alert (num)
+}
+
+function countQ(){
+  var quotes = document.getElementById('quotes').getElementsByTagName('p')
+  var num = quotes.length
+  alert (num)
+}
+
+function countE(){
+  var episodes = document.getElementById('bests').getElementsByTagName('p')
+  var num = episodes.length
+  alert (num)
+}
